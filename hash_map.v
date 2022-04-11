91ccaae12a0374cb7a5c792dfd76e36b402de48c
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2022-04-11 11:23:30 +0200

    Readd assert statement

--- hash_map.v
+++ hash_map.v
@@ -113,6 +113,7 @@ pub fn (mut m HashMap<K, V>) set<K, V>(key K, value V) {
 		m.rehash()
 	}
 	mut bucket := m.buckets[index]
+	assert voidptr(bucket) != 0
 	for pair in bucket.pairs {
 		mut p := pair
 		if p.key.equals(key) {
